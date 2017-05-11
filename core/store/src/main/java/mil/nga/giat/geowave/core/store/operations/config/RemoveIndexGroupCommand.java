a623ce5881168bf763b30ddc16a6a9c76e249542
mak438 <mak438@cornell.edu>
2017-05-11 12:32:40 -0400

    Geowave rest project (#1097)
    * Add basic restlet server implementation
    
     - Add all available commands as routes
    
    * Fix build failure
    
     - Unused variable was in RestServer.java
    
    * excluded restlet dependencies
    
    * ListCommand added to rest server, as an example
    
    * More work trying to separate out the restlet versions
    
    * Fix for Jackson Extension
    
    * Setup geowave/config/rmindexgrp
    
    * Add rest annotations to RemoveStoreCommand
    
    * Add config/rmindex
    
    * use shaded restlet
    
    * Add rest endpoint functionality in class hierarchy
    
    * Add computeResults stubs
    
    * Rest setcommand
    
     - Add restlet annotations to SetCommand class
    
    * setup geowave/config/addstore and cpstore
    
    * Fix search pattern in abstract remove command
    
    * Add additional dependencies for RestServer to detect operations
    
    * Fix findbugs error on compareTo
    
    * AddIndexGroup and copyIndex: endpoint
    
    * Rest endpoints to unimplemented paths
    
    * Add annotations to implemented endpoints
    
    * Log all exceptions, helpful for unit testing
    
    * Format default server page and run code formatting
    
    * Added authentication
    
    * Add rest annotations to RemoveStoreCommand
    
    * Rest setcommand
    
     - Add restlet annotations to SetCommand class
    
    * setup geowave/config/addstore and cpstore
    
    add form to addstore
    
    working on restservertest,having problem with post
    
    * RestServerTest with form
    
    * added unit tests for addstore, cpstore, rmstore commands
    
    * unit tests for set and list commands
    
    Add config file in the request
    
    fixing unit test for list command
    
    AddIndexGroup and copyIndex: endpoint
    
    * added unit test for addindexgrp and rmindexgrp
    
    * Added authentication
    
    added authentication to unit tests
    
    * add ignore annotation for index related commands
    
    RestServerTest to ensure config directory exists, for the password file
    
    * use list to test if removed store successfully
    
    * Automatically read parameters into the command classes
    
    * MemoryStoreFactoryFamily to be injected via Spi for AddStore command, removing some memory-specific logic we had to add to this one.
    
    * geowave/config/addindex and geowave/config/copyindex (#19)
    
     geowave/config/addindex and geowave/config/cpindex (+ test for geowave/config/rmindex)
    
    * File upload, storing in temp directory (#20)
    
    * add /geowave/config/addindexgrp (#22)
    
    * add /geowave/config/addindexgrp
    
    * Added documentation
    
    * Compute results in ingest
    
    * Add ingest unit tests
    
    * Fix tmp dir missing forwardslash on linux
    
    * Get absolute file path

--- core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/RemoveIndexGroupCommand.java
+++ core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/RemoveIndexGroupCommand.java
@@ -1,14 +1,25 @@
 package mil.nga.giat.geowave.core.store.operations.config;
 
+import org.shaded.restlet.representation.Representation;
+
+import java.io.File;
+
+import org.shaded.restlet.data.Form;
+import org.shaded.restlet.data.Status;
+import org.shaded.restlet.resource.Post;
+
 import com.beust.jcommander.Parameters;
 
 import mil.nga.giat.geowave.core.cli.annotations.GeowaveOperation;
 import mil.nga.giat.geowave.core.cli.api.Command;
 import mil.nga.giat.geowave.core.cli.api.OperationParams;
 import mil.nga.giat.geowave.core.cli.operations.config.ConfigSection;
+import mil.nga.giat.geowave.core.cli.operations.config.options.ConfigOptions;
+import mil.nga.giat.geowave.core.cli.parser.ManualOperationParams;
 import mil.nga.giat.geowave.core.store.operations.remote.options.IndexGroupPluginOptions;
+import static mil.nga.giat.geowave.core.cli.annotations.GeowaveOperation.RestEnabledType.*;
 
-@GeowaveOperation(name = "rmindexgrp", parentOperation = ConfigSection.class)
+@GeowaveOperation(name = "rmindexgrp", parentOperation = ConfigSection.class, restEnabled = POST)
 @Parameters(commandDescription = "Remove index group from Geowave configuration")
 public class RemoveIndexGroupCommand extends
 		AbstractRemoveCommand implements
@@ -16,14 +27,18 @@ public class RemoveIndexGroupCommand extends
 {
 
 	@Override
-	public void execute(
+	public Void computeResults(
 			OperationParams params ) {
 
 		// Search for properties relevant to the given name
-		String pattern = IndexGroupPluginOptions.getIndexGroupNamespace(getEntryName());
-		super.execute(
-				params,
-				pattern);
+		pattern = IndexGroupPluginOptions.getIndexGroupNamespace(getEntryName());
+		return super.computeResults(params);
 
+	}
+
+	@Override
+	public void execute(
+			OperationParams params ) {
+		computeResults(params);
 	}
 }
